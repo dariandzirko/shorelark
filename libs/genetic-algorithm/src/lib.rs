48550b7c357d7ccc8c8bad8f290751d31d7d382f
Darian <dariandzirko@gmail.com>
2024-02-01 12:55:11 -0700

    Finally starting the genetic algo part

---
+++ libs/genetic-algorithm/src/lib.rs
@@ -0,0 +1,27 @@
+pub struct GeneticAlgorithm;
+
+pub trait Individual {
+    fn fitness(&self) -> f32;
+}
+
+impl GeneticAlgorithm {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn evolve<I>(&self, population: &[I]) -> Vec<I>
+    where
+        I: Individual,
+    {
+        assert!(!population.is_empty());
+
+        (0..population.len())
+            .map(|_| {
+                //TODO selection
+                //TODO crossover
+                //TODO mutation
+                todo!()
+            })
+            .collect()
+    }
+}
