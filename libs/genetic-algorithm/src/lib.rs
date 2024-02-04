a5e7e4cd0ce72f2bbbfe7fe3dc6ace789f753928
Darian <dariandzirko@gmail.com>
2024-02-04 15:14:46 -0700

    Genetic algorithm basics exist but the tests do not. The code is also very messy

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -1,7 +1,16 @@
-use rand::{seq::SliceRandom, RngCore};
+#![feature(impl_trait_in_assoc_type)]
+
+use core::panic;
+use std::{ops::Index, str::CharIndices};
+
+use rand::{seq::SliceRandom, Rng, RngCore};
 
 pub trait Individual {
     fn fitness(&self) -> f32;
+
+    fn chromosome(&self) -> &Chromosome;
+
+    fn create(chromosome: Chromosome) -> Self;
 }
 
 pub trait SelectionMethod {
@@ -29,14 +38,29 @@ impl SelectionMethod for RouletteWheelSelection {
     }
 }
 
-pub struct GeneticAlgorithm;
+pub struct GeneticAlgorithm<S> {
+    selection_method: S,
+    crossover_method: Box<dyn CrossOverMethod>,
+    mutation_method: Box<dyn MutationMethod>,
+}
 
-impl GeneticAlgorithm {
-    pub fn new() -> Self {
-        Self
+impl<S> GeneticAlgorithm<S>
+where
+    S: SelectionMethod,
+{
+    pub fn new(
+        selection_method: S,
+        crossover_method: impl CrossOverMethod + 'static,
+        mutation_method: impl MutationMethod + 'static,
+    ) -> Self {
+        Self {
+            selection_method,
+            crossover_method: Box::new(crossover_method),
+            mutation_method: Box::new(mutation_method),
+        }
     }
 
-    pub fn evolve<I>(&self, population: &[I]) -> Vec<I>
+    pub fn evolve<I>(&self, rng: &mut dyn RngCore, population: &[I]) -> Vec<I>
     where
         I: Individual,
     {
@@ -44,12 +68,126 @@ impl GeneticAlgorithm {
 
         (0..population.len())
             .map(|_| {
-                //TODO selection
-                //TODO crossover
-                //TODO mutation
-                todo!()
+                let parent_a = self.selection_method.select(rng, &population).chromosome();
+
+                let parent_b = self.selection_method.select(rng, &population).chromosome();
+
+                let mut child = self.crossover_method.crossover(rng, parent_a, parent_b);
+
+                self.mutation_method.mutate(rng, &mut child);
+                I::create(child)
             })
             .collect()
+    }
+}
+
+pub struct Chromosome {
+    genes: Vec<f32>,
+}
+
+impl Chromosome {
+    pub fn len(&self) -> usize {
+        self.genes.len()
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &f32> {
+        self.genes.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut f32> {
+        self.genes.iter_mut()
+    }
+}
+
+impl Index<usize> for Chromosome {
+    type Output = f32;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.genes[index]
+    }
+}
+
+impl FromIterator<f32> for Chromosome {
+    fn from_iter<T: IntoIterator<Item = f32>>(iter: T) -> Self {
+        Self {
+            genes: iter.into_iter().collect(),
+        }
+    }
+}
+
+impl IntoIterator for Chromosome {
+    type Item = f32;
+    type IntoIter = impl Iterator<Item = f32>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.genes.into_iter()
+    }
+}
+
+pub trait CrossOverMethod {
+    fn crossover(
+        &self,
+        rng: &mut dyn RngCore,
+        parent_a: &Chromosome,
+        parent_b: &Chromosome,
+    ) -> Chromosome;
+}
+
+pub struct UniformCrossover;
+
+impl UniformCrossover {
+    pub fn new() -> Self {
+        Self
+    }
+}
+
+impl CrossOverMethod for UniformCrossover {
+    fn crossover(
+        &self,
+        rng: &mut dyn RngCore,
+        parent_a: &Chromosome,
+        parent_b: &Chromosome,
+    ) -> Chromosome {
+        assert_eq!(parent_a.len(), parent_b.len());
+
+        parent_a
+            .iter()
+            .zip(parent_b.iter())
+            .map(|(&a, &b)| if rng.gen_bool(0.5) { a } else { b })
+            .collect()
+    }
+}
+
+pub trait MutationMethod {
+    fn mutate(&self, rng: &mut dyn RngCore, child: &mut Chromosome);
+}
+
+pub struct GaussianMutation {
+    chance: f32,
+
+    coefficient: f32,
+}
+
+impl GaussianMutation {
+    pub fn new(chance: f32, coefficient: f32) -> Self {
+        assert!(chance >= 0.0 && chance <= 1.0);
+
+        Self {
+            chance,
+            coefficient,
+        }
+    }
+}
+
+impl MutationMethod for GaussianMutation {
+    fn mutate(&self, rng: &mut dyn RngCore, child: &mut Chromosome) {
+        for gene in child.genes.iter_mut() {
+            let sign = if rng.gen_bool(0.5) { -1.0 } else { 1.0 };
+
+            if rng.gen_bool(self.chance as _) {
+                *gene += sign * self.coefficient * rng.gen::<f32>();
+            }
+        }
     }
 }
 
@@ -69,6 +207,14 @@ impl TestIndividual {
 impl Individual for TestIndividual {
     fn fitness(&self) -> f32 {
         self.fitness
+    }
+
+    fn chromosome(&self) -> &Chromosome {
+        panic!("not supported for TestIndividual")
+    }
+
+    fn create(chromosome: Chromosome) -> Self {
+        panic!("not supported for TestIndidivual")
     }
 }
 
@@ -93,7 +239,13 @@ mod test {
             TestIndividual::new(4.0),
         ];
 
-        let mut actual_histogram = BTreeMap::new();
+        let mut actual_histogram: BTreeMap<i32, _> = (0..1000)
+            .map(|_| method.select(&mut rng, &population))
+            .fold(Default::default(), |mut histogram, individual| {
+                *histogram.entry(individual.fitness() as _).or_default() += 1;
+
+                histogram
+            });
 
         for _ in 0..1000 {
             let fitness = method.select(&mut rng, &population).fitness() as i32;
